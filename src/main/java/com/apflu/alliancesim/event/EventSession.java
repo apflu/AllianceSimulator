439724a727008cabe481a251d17763651fc4ca40
summer <urvqp@student.kit.edu>
2024-12-02 05:13:11 +0100

    Merge remote-tracking branch 'origin/master' attempted to finish the work in issue 3 not sure if i understand your description correctly but good to have a start
    # Conflicts:
    #	.idea/checkstyle-idea.xml
    #	src/main/java/com/apflu/alliancesim/game/space/Asteroid.java

--- src/main/java/com/apflu/alliancesim/event/EventSession.java
+++ src/main/java/com/apflu/alliancesim/event/EventSession.java
@@ -3,23 +3,34 @@ package com.apflu.alliancesim.event;
 import com.apflu.alliancesim.player.Alliance;
 
 public class EventSession {
+    private final Alliance alliance;
+    private final Object triggerSource;
+    private final GameEventLine eventLine;
+    private final Object eventLineSource;
+
+    public EventSession(Alliance alliance, Object triggerSource, GameEventLine eventLine, Object eventLineSource) {
+        this.alliance = alliance;
+        this.triggerSource = triggerSource;
+        this.eventLine = eventLine;
+        this.eventLineSource = eventLineSource;
+    }
     public Alliance getAlliance() {
         // TODO
-        return null;
+        return alliance;
     }
 
     public Object getTriggerSource() {
         // TODO
-        return null;
+        return triggerSource;
     }
 
     public GameEventLine getEventLine() {
         // TODO
-        return null;
+        return eventLine;
     }
 
     public Object getEventLineSource() {
         // TODO
-        return null;
+        return eventLineSource;
     }
 }
